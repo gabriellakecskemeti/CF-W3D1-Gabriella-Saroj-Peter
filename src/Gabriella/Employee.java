51d1d07a39d3395e973af5ed8c3d8e2271465b42
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2020-11-09 11:57:09 +0100

    Employee

---
+++ src/Gabriella/Employee.java
@@ -0,0 +1,84 @@
+package Gabriella;
+
+import javafx.util.converter.IntegerStringConverter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Employee {
+    private String name;
+    private String surName;
+    private int yearOfBirth;
+    private double salary;           // salary in EUR
+
+
+    public Employee(String name, String surName, int yearOfBirth, double salary) {
+        this.name = name;
+        this.surName = surName;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+    }
+
+    public String[] getEmployee(){
+        String[] employee = {name, surName, String.valueOf(yearOfBirth),String.valueOf(salary)  };
+
+        return employee;
+    }
+
+    public void setEmployee(String name, String surName, int yearOfBirth, int salary){
+        this.name = name;
+        this.surName = surName;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void printOutEmployee(){
+        System.out.println(name+"  "+surName+"         Year of Birth: "+yearOfBirth+"    Salary: "+salary);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/Gabriella/Main.java
+++ src/Gabriella/Main.java
@@ -1,4 +1,40 @@
 package Gabriella;
 
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        //Create Employee objects
+        ArrayList<Employee> allEmployee= new ArrayList<>();
+
+        allEmployee.add(new Employee("Oliver", "Hukapuk", 1980, 6000));
+        allEmployee.add(new Employee("Beatrix", "Kiss", 1986, 5000));
+        allEmployee.add(new Employee("Peter", "Pan", 1988, 3000));
+        allEmployee.add(new Employee("Kathrin", "Meyer", 1978, 3900));
+        allEmployee.add(new Employee("Michaela", "Lang", 1972, 4100));
+
+        Iterator itr= allEmployee.iterator();
+        System.out.println(itr);
+
+        System.out.println("***********Print out one Employee****");
+        allEmployee.get(0).printOutEmployee();
+
+        allEmployee.get(0).setSurName("Schnarch");
+        allEmployee.get(0).printOutEmployee();
+
+        System.out.println("\n***********Print out all Employee****");
+        while(itr.hasNext()) {
+            Employee ee =(Employee) itr.next();
+            //System.out.println("ee: "+ee);
+            ee.printOutEmployee();
+        }
+
+
+    }
+
+
+
 }
